a59381251015e68b031e9540d967754b62095216
EXlena7-7 <marjor071101@gmail.com>
2024-05-10 22:17:28 -0400

    💫 mejorando

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
+  const {currentUser} = React.useContext(AuthContext);
+
   return (
     <div className='navbar'>
         <span className='logo'>Marjo Chat</span>
         <div className='user'>
-            <img src="https://avatars.githubusercontent.com/u/109706655?s=400&u=191a43a1d05d3b143524f014541da83c7a55667c&v=4" alt=""/>
-            <span>John</span>
+            <img src={currentUser.photoURL} alt=""/>
+            <span>{currentUser.displayName}</span>
             <button onClick={()=>signOut(auth)}>logout</button>
         </div>
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { collection, query, where } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = () =>{
+    const q = query(
+      collection(db, "users"), 
+      where("displayName", "==", username)
+    );
+  };
+
+  const handleKey = (e) =>{
+    e.code === "Enter" && handleSearch();
+  }
   return (
     <div className='search'>
       <div className='searchForm'>
-        <input type="text" placeholder="Find a user"/>
+        <input 
+        type="text" 
+        placeholder="Find a user"
+        onKeyDown={handleKey} 
+        onChange={e=>setUsername(e.target.value)}/>
         
       </div>
       <div className='userChat'>
